c01b02ba39813e51cd90eb39ca11dcc77cfe3e43
etane bigot <etane.bigot@epfedu.fr>
2024-11-29 15:27:13 +0100

    miniprojet

--- MiniProjet_Lightoff/src/miniprojet_lightoff/CelluleLumineuse.java
+++ MiniProjet_Lightoff/src/miniprojet_lightoff/CelluleLumineuse.java
@@ -10,4 +10,45 @@ package miniprojet_lightoff;
  */
 public class CelluleLumineuse {
     private Boolean etat;
+    
+    public CelluleLumineuse(){
+        this.etat=false;
+    }
+    
+    public void activerCellule(){
+        if(etat==false){
+            this.etat=true;
+        }else{
+            this.etat=false;
+        }
+    }
+    
+    public void eteindreCellule(){
+        if(etat==true){
+            this.etat=false; 
+        }
+    }
+    
+    public Boolean estEteint(){
+        if(etat==true){
+            return false;
+        }else{
+            return true;
+        }
+    }
+    
+    public Boolean getEtat(){
+        if(etat==true){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "CelluleLumineuse{" + "etat=" + etat + '}';
+    }
+    
+     
 }
