6eed89541f4d3b190d4a792f8d3025679acde314
etane bigot <etane.bigot@epfedu.fr>
2024-11-29 17:13:58 +0100

    miniprojet etane: classe CellulleLumineuse: terminée classe Partie: Partie et initialiserPartie: terminée 					   lancerPartie: en cours

--- MiniProjet_Lightoff/src/miniprojet_lightoff/Partie.java
+++ MiniProjet_Lightoff/src/miniprojet_lightoff/Partie.java
@@ -6,9 +6,30 @@ package miniprojet_lightoff;
 
 /**
  *
- * @author damie
+ * @author etane
  */
 public class Partie {
-    GrilleDeJeu grille = new GrilleDeJeu();
+    private GrilleDeJeu grille;
     int nbCoups;
+
+    public Partie(){
+        grille=new GrilleDeJeu(6,6);
+        this.nbCoups=0;
+    }
+    
+    public void initialiserPartie(){
+        grille.melangerMatriceAleatoirement(10);
+        this.nbCoups=0;  
+    }
+    
+    public void lancerPartie(){
+        initialiserPartie();
+        while(cellulesToutesEteintes()!=true){
+            System.out.println("veuillez saisir la ligne/colonnne/diagonale a inversser");
+            //scanner(valeur saisie)
+            //activer l/c/diag(utiliser méthode classeGDJ)
+            //maj grille
+        }
+        //afficher nombre de coup
+    }
 }
